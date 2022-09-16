678ee15276215566bfab4aad1b434d2e53f90c58
Vladimir <vovakozlouskiy@gmail.com>
2022-09-16 14:52:48 +0300

    Implemented two points drag

--- src/app/Components/Canvas/canvas.component.ts
+++ src/app/Components/Canvas/canvas.component.ts
@@ -4,8 +4,7 @@ import {SVG} from '@svgdotjs/svg.js'
 import '@svgdotjs/svg.panzoom.js'
 import '@svgdotjs/svg.draggable.js'
 import {Rect} from "@svgdotjs/svg.js";
-import {G} from "@svgdotjs/svg.js";
-import { Element } from "@svgdotjs/svg.js";
+import {G, Text} from "@svgdotjs/svg.js";
 
 
 @Component({
@@ -21,8 +20,8 @@ export class CanvasComponent implements OnInit, OnDestroy {
 
   private resizeGridMargin = 8;
 
-  private textMargin = 32;
-  private perimeterInfo!: Element;
+  private perimeterInfo!: Text;
+  private draggable!: G;
 
   ngOnInit(): void {
     const width = "100%"
@@ -44,12 +43,11 @@ export class CanvasComponent implements OnInit, OnDestroy {
 
     this.groupWithPinButtons.add(this.rectangle)
 
-    this.addPerimeterInfoText(this.groupWithPinButtons)
+    this.addPerimeterInfoText(this.canvas)
 
-    this.groupWithPinButtons.draggable()
+    this.draggable = this.groupWithPinButtons.draggable()
 
     this.canvas.add(this.groupWithPinButtons)
-
 
     this.addListeners()
   }
@@ -105,13 +103,19 @@ export class CanvasComponent implements OnInit, OnDestroy {
 
     topLeftCircle.attr({cursor : 'nw-resize'})
 
+    this.addTopLeftListener(topLeftCircle)
+
     const topRightCircle = getResizePoint(topRightX, topRightY)
 
     topRightCircle.attr({cursor : 'ne-resize'})
 
+    this.addTopRightListener(topRightCircle)
+
     const bottomLeftCircle = getResizePoint(bottomRightX, bottomRightY)
 
     bottomLeftCircle.attr({cursor : 'se-resize'})
+
+    this.addBottomLeftListener(bottomLeftCircle)
 
     const bottomRightCircle = getResizePoint(bottomLeftX, bottomLeftY)
 
@@ -125,15 +129,252 @@ export class CanvasComponent implements OnInit, OnDestroy {
     this.groupWithPinButtons.add(bottomRightCircle)
   }
 
-  private addPerimeterInfoText(element : G){
+  private addPerimeterInfoText(element : Svg){
     const rectanglePerimeter = (this.rectangle.width() as number) * (this.rectangle.height() as number)
     this.perimeterInfo = SVG()
       .text(`Perimeter is ${rectanglePerimeter}`)
-      .move(
-        (element.width() as number) + this.textMargin,
-        (element.y() as number) + (element.height() as number) / 2)
+      .move(48, 48)
 
     element.add(this.perimeterInfo)
   }
+
+  private updatePerimeterInfo(newPerimeter : number){
+    this.perimeterInfo.text(`Perimeter is ${newPerimeter}`)
+  }
+
+  private addTopLeftListener(topLeftCircle : Circle){
+    let x: number
+    let y: number
+    let width: number
+    let height: number
+
+    const onMouseMove = (event : any) => {
+      event.preventDefault()
+
+      if (event.clientX < x){
+        const newWidth = width + (x - event.clientX)
+
+        if (newWidth < 100)
+          return
+
+        this.groupWithPinButtons.x(event.clientX)
+        this.groupWithPinButtons.width(newWidth)
+      }
+
+      if (event.clientX > x){
+        const newWidth = width - (event.clientX - x)
+
+        if (newWidth < 100)
+          return
+
+        this.groupWithPinButtons.x(event.clientX)
+        this.groupWithPinButtons.width(newWidth)
+      }
+
+      if (event.clientY < y){
+        const newHeight = height + (y - event.clientY)
+
+        if (newHeight < 100)
+          return
+
+        this.groupWithPinButtons.y(event.clientY)
+        this.groupWithPinButtons.height(newHeight)
+      }
+
+      if (event.clientY > y){
+        const newHeight = height - (event.clientY - y)
+
+        if (newHeight < 100)
+          return
+
+        this.groupWithPinButtons.y(event.clientY)
+        this.groupWithPinButtons.height(newHeight)
+      }
+
+      const rectWidth = this.rectangle.node.getBoundingClientRect().width
+      const rectHeight = this.rectangle.node.getBoundingClientRect().height
+
+      this.updatePerimeterInfo(rectWidth * rectHeight)
+    }
+
+    const onMouseUp = (event : any) => {
+      event.preventDefault()
+      window.removeEventListener("onmousemove", onMouseMove)
+      window.onmousemove = null
+    }
+
+    const onMouseDown = (event : any) => {
+      event.preventDefault()
+      x = this.groupWithPinButtons.x() as number
+      y = this.groupWithPinButtons.y() as number
+      width = this.groupWithPinButtons.width() as number
+      height = this.groupWithPinButtons.height() as number
+
+      window.onmousemove = onMouseMove
+
+      window.onmouseup = onMouseUp
+    }
+
+    topLeftCircle.node.onmousedown = onMouseDown
+  }
+
+  private addTopRightListener(topRightCircle : Circle){
+    let x: number
+    let y: number
+    let width: number
+    let height: number
+
+    const onMouseMove = (event : any) => {
+      event.preventDefault()
+
+
+      if (event.clientX > x + width){
+        const newWidth = event.clientX - x
+
+        if (newWidth < 100)
+          return
+
+        this.groupWithPinButtons.x(x)
+        this.groupWithPinButtons.width(newWidth)
+      }
+
+      if (event.clientX < x + width){
+        const newWidth = event.clientX - x
+
+        if (newWidth < 100)
+          return
+
+        this.groupWithPinButtons.x(x)
+        this.groupWithPinButtons.width(newWidth)
+      }
+
+      if (event.clientY < y){
+        const newHeight = y - event.clientY + height
+
+        if (newHeight < 100)
+          return
+
+        this.groupWithPinButtons.y(event.clientY)
+        this.groupWithPinButtons.height(newHeight)
+      }
+
+      if (event.clientY > y){
+        const newHeight = height - (event.clientY - y)
+
+        if (newHeight < 100)
+          return
+
+        this.groupWithPinButtons.y(event.clientY)
+        this.groupWithPinButtons.height(newHeight)
+      }
+
+      const rectWidth = this.rectangle.node.getBoundingClientRect().width
+      const rectHeight = this.rectangle.node.getBoundingClientRect().height
+
+      this.updatePerimeterInfo(rectWidth * rectHeight)
+    }
+
+    const onMouseUp = (event : any) => {
+      event.preventDefault()
+      window.removeEventListener("onmousemove", onMouseMove)
+      this.draggable.off("beforedrag dragmove")
+      window.onmousemove = null
+    }
+
+    const onMouseDown = (event : any) => {
+      this.draggable.on("beforedrag dragmove", (event) => {
+        event.preventDefault()
+      })
+      event.preventDefault()
+      x = this.groupWithPinButtons.x() as number
+      y = this.groupWithPinButtons.y() as number
+      width = this.groupWithPinButtons.width() as number
+      height = this.groupWithPinButtons.height() as number
+
+      window.onmousemove = onMouseMove
+
+      window.onmouseup = onMouseUp
+    }
+
+    topRightCircle.node.onmousedown = onMouseDown
+  }
+
+  private addBottomLeftListener(bottomLeftCircle : Circle){
+    let x: number
+    let y: number
+    let width: number
+    let height: number
+
+    const onMouseMove = (event : any) => {
+      event.preventDefault()
+
+      if (event.clientX < x){
+        const newWidth = width + (x - event.clientX)
+
+        if (newWidth < 100)
+          return
+
+        this.groupWithPinButtons.x(event.clientX)
+        this.groupWithPinButtons.width(newWidth)
+      }
+
+      if (event.clientX > x){
+        const newWidth = width - (event.clientX - x)
+
+        if (newWidth < 100)
+          return
+
+        this.groupWithPinButtons.x(event.clientX)
+        this.groupWithPinButtons.width(newWidth)
+      }
+
+      if (event.clientY < y + height){
+        const newHeight = y - event.clientY
+
+        if (newHeight < 100)
+          return
+
+        this.groupWithPinButtons.y(event.clientY)
+        this.groupWithPinButtons.height(newHeight)
+      }
+
+      if (event.clientY > y + height){
+        const newHeight = event.clientY - y
+
+        if (newHeight < 100)
+          return
+
+        this.groupWithPinButtons.y(event.clientY)
+        this.groupWithPinButtons.height(newHeight)
+      }
+
+      const rectWidth = this.rectangle.node.getBoundingClientRect().width
+      const rectHeight = this.rectangle.node.getBoundingClientRect().height
+
+      this.updatePerimeterInfo(rectWidth * rectHeight)
+    }
+
+    const onMouseUp = (event : any) => {
+      event.preventDefault()
+      window.removeEventListener("onmousemove", onMouseMove)
+      window.onmousemove = null
+    }
+
+    const onMouseDown = (event : any) => {
+      event.preventDefault()
+      x = this.groupWithPinButtons.x() as number
+      y = this.groupWithPinButtons.y() as number
+      width = this.groupWithPinButtons.width() as number
+      height = this.groupWithPinButtons.height() as number
+
+      window.onmousemove = onMouseMove
+
+      window.onmouseup = onMouseUp
+    }
+
+    bottomLeftCircle.node.onmousedown = onMouseDown
+  }
+
+
 
 }
